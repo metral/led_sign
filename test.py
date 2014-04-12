4e018197cd8975c23ebe4ac044b70e3a9c78b443
Mike Metral <metral@gmail.com>
2014-04-12 15:17:03 -0700

    Add test

---
+++ test.py
@@ -0,0 +1,9 @@
+from client import SignClient
+
+multiline_test = []
+
+top_line = "Python rocks!"
+bottom_line = "Ruby is cool too :)"
+multiline_test.append([top_line, bottom_line])
+
+SignClient().send_text_to_sign(multiline_test)
