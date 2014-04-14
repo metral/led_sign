62b7e26c8859f0e31da638aaf83c7f5ff2ea4b39
Mike Metral <metral@gmail.com>
2014-04-14 16:59:17 -0700

    Add led_sign_path

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ bottom_line = "Ruby is cool too :)"
 multiline_test = [top_line, bottom_line]
 
 pwd = os.path.dirname(os.path.realpath(__file__))
-glyphs_path = '/'.join([pwd, 'glyphs'])
+led_sign_path = '/'.join([pwd, 'led_sign']) 
+glyphs_path = '/'.join([led_sign_path, 'glyphs'])
 
-SignClient(glyphs_path).send_text_to_sign(multiline_test)
+SignClient(glyphs_path, led_sign_path).send_text_to_sign(multiline_test)
